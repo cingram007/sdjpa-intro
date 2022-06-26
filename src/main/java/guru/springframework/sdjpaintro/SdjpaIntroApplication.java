6039aa4ec4da39c541059dad018f644dbc62b4d5
Christopher Ingram-Reid <christopher.ingram-reid@camh.ca>
2022-06-26 14:56:34 -0400

    added documentation notes

--- src/main/java/guru/springframework/sdjpaintro/SdjpaIntroApplication.java
+++ src/main/java/guru/springframework/sdjpaintro/SdjpaIntroApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SdjpaIntroApplication {
 
+    //Note: this run method will run the run method found on the class path
     public static void main(String[] args) {
         SpringApplication.run(SdjpaIntroApplication.class, args);
     }
