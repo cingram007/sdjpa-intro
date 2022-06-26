0280cc08c322a3a62b584a8c488664855e99dcf9
Christopher Ingram-Reid <christopher.ingram-reid@camh.ca>
2022-06-26 15:00:53 -0400

    Autowired Book repository and wrote test

--- src/test/java/guru/springframework/sdjpaintro/SdjpaIntroApplicationTests.java
+++ src/test/java/guru/springframework/sdjpaintro/SdjpaIntroApplicationTests.java
@@ -1,10 +1,25 @@
 package guru.springframework.sdjpaintro;
 
+import guru.springframework.sdjpaintro.repositories.BookRepository;
+
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*; //static import assertJ very nice assertion library
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//Note: this class is created for us by the Spring Boot initializer
+//      out of the box.
 @SpringBootTest
 class SdjpaIntroApplicationTests {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testBookRepository(){
+        long count = bookRepository.count();
+        assertThat(count).isGreaterThan(0); //
+    }
 
     @Test
     void contextLoads() {
