a60995543d73028a25e504354fb898955a72093c
Christopher Ingram-Reid <Chris.Ingram-Reid@camh.ca>
2022-07-04 14:12:55 -0400

    add data to the MySQL instance schema

--- src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
@@ -2,11 +2,13 @@ package guru.springframework.sdjpaintro.bootstrap;
 
 import guru.springframework.sdjpaintro.domain.Book;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.boot.CommandLineRunner;
 
 //tis is huge. What makes a class a Spring Component is the @Component tag
 // plus it must implement the CommandLineRunner Interface
+@Profile({"local", "default"})
 @Component
 public class DataInitializer implements CommandLineRunner{
 
@@ -18,12 +20,14 @@ public class DataInitializer implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
+
+        bookRepository.deleteAll(); //added Lesson 33
+
         Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse");
-        System.out.println("Id: " + bookDDD.getId());
-
+        //System.out.println("Id: " + bookDDD.getId());
         Book savedDDD = bookRepository.save(bookDDD);
-        System.out.println("Id: " + savedDDD.getId());
 
+        //System.out.println("Id: " + savedDDD.getId());
         Book bookSIA = new Book("Spring in Action", "234234", "OReilly");
         Book savedSIA = bookRepository.save(bookSIA);
 
